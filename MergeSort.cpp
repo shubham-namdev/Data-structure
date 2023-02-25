8a4c108c0514c5627213b91cab14b7d1118396b7
Shubham Namdev <85869268+shubham-namdev@users.noreply.github.com>
2023-02-25 11:29:56 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -35,14 +35,16 @@ void mergeVect(vector<int>& arr, int s, int mid, int e){
 
     for(int i=s; i<=e; ++i){
 
-        arr[left] = temp[left-s]; 
+        //arr[left] = temp[left-s];
+        arr[left = temp[i-s];
     }
 
 }
 
 void mergeSort(vector<int>& arr, int s, int e){
     if(s<e){
-        int mid = s+(e-s)/2;
+        //int mid = s+(e-s)/2;
+        int mid = (s+e)/2;
         mergeSort(arr,s,mid);
         mergeSort(arr, mid + 1, e);
         mergeVect(arr, s, mid, e);
